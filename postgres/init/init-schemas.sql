a1d22a73fc7e1dc3180f47c9c26d1513cbb13fb4
takeshiki <dimav2206@gmail.com>
2025-10-19 23:43:21 +0300

    removed read_model_db

--- postgres/init/init-schemas.sql
+++ postgres/init/init-schemas.sql
@@ -1,11 +1,8 @@
 CREATE DATABASE events_db;
 
-CREATE DATABASE read_model_db;
-
 \c events_db;
 CREATE SCHEMA IF NOT EXISTS ttk_collector;
 
 CREATE SCHEMA IF NOT EXISTS fb_collector;
 
-\c read_model_db;
-CREATE SCHEMA IF NOT EXISTS public;
+CREATE SCHEMA IF NOT EXISTS reporting;
